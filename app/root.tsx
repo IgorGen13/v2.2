8c1e51ffc53cbca78ed137a6f4f5fe96425fa50e
Igor <ase@expertme.ru>
2025-01-10 23:16:06 +0200

    2

--- app/root.tsx
+++ app/root.tsx
@@ -6,7 +6,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
 			<head>
 				<meta charSet="utf-8" />
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
-				<script src="https://cdn.dashjs.org/v3.1.0/dash.all.debug.js" />
+				<script src="http://cdn.dashjs.org/v3.1.0/dash.all.min.js" />
 				<Meta />
 				<Links />
 			</head>
