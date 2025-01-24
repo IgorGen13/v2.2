a4f85ab2c5e3e446c2262de901cdab4cd93747a0
Igor <ase@expertme.ru>
2025-01-24 11:12:57 +0200

    fix hls

--- app/root.tsx
+++ app/root.tsx
@@ -6,7 +6,8 @@ export function Layout({ children }: { children: React.ReactNode }) {
 			<head>
 				<meta charSet="utf-8" />
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
-				<script src="https://cdn.dashjs.org/v3.1.0/dash.all.min.js" />
+				{/* <script src="https://cdn.dashjs.org/v3.1.0/dash.all.min.js" /> */}
+				<script src="https://cdn.jsdelivr.net/npm/hls.js@latest"></script>
 				<Meta />
 				<Links />
 			</head>
